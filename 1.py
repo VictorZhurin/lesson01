b737f9af99cc4bda2171b79db02e14f557a9f535
Victor Zhurin <vityazhurinzhurin@gmail.com>
2019-12-18 21:32:28 +0300

    Задания на первый вебинар

---
+++ 1.py
@@ -0,0 +1,11 @@
+a = 5
+b = 6
+print(a + b)
+print(a * b)
+print(a / b)
+
+
+age = int(input('Введите ваш возраст: '))
+name = input('Ваше имя: ')
+weight = input('Ваш вес: ')
+print('Ваш возраст: {}, Ваше имя: {}, Ваш вес: {}'.format(age,name,weight))
